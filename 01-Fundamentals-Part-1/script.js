b129603c937e25b852326550feeaa4a6eb952d2a
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-01 14:48:46 +0530

    upload

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,9 @@
+let firstName = "jones";
+console.log(firstName);
+
+let jones_matilda="JM";
+let myfirstjob = "coder";
+let mycurrentjob ="Teacher";
+console.log(myFirstjob);
+let $function = 27;
+let PI =3.1415;
