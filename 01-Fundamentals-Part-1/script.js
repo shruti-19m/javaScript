021e84f5ed33fa7f590cb9ec89245857d18f7a4e
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-01 22:01:04 +0530

    (upload

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -135,4 +135,16 @@ console.log(BMIMark, BMIJohn);
     console.log("John's BMI is higher than Marks's")
   }*/
 
-    
+
+const inputYear = '1991';
+console.log(Number(inputYear),inputYear);
+console.log(Number(inputYear) + 10);
+
+console.log(Number('jones'));
+console.log(typeof NaN);
+
+console.log(String(23),23);
+
+let n ='1' +1;
+n = n-1;
+console.log(n);
