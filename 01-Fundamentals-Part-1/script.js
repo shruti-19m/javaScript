01bdb7d0cdc5f60d4eab118c68396e6c17eb8444
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-01 21:46:05 +0530

    upload

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -120,3 +120,17 @@ console.log(century);
 */
 
 
+const massMark = 95;
+const massJohn = 85;
+const heightMark = 1.88;
+const heightJohn = 1.76;
+
+const BMIMark = massMark/heightMark **2;
+const BMIJohn = massJohn/(heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+  if(BMIMark>BMIJohn)
+  {
+    console.log("Mark's BMI is higher than John's")
+  } else {
+    console.log("John's BMI is higher than Marks's")
+  }
