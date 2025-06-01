41e5a5ce146f1a8e5e61c2d4bac1f8cf45f94c13
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-01 18:27:19 +0530

    update

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -11,6 +11,8 @@ let javascriptIsFun = true;
 console.log(javascriptIsFun);
 console.log(typeof true);
 console.log(typeof true);*/
+
+
 /*let Country = "India";
 console.log(country);
 let continent ="Asia";
@@ -18,3 +20,15 @@ console.log(continent);
 let population = 4700;
 console.log(population);let country ="India";*/
 
+
+console.log(typeof 'jones');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
