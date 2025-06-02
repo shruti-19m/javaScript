94a0f0fc0602533c0512177e378d8c476b19a341
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-03 01:38:06 +0530

    update

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -370,3 +370,25 @@ console.log(ages);
 
 */
 
+
+const jones = [
+    'jones',
+    'Schmedtmann',
+    2025-2005,
+    'Student',
+    ['Shruti','Sourav','Anuska',],
+];
+
+//0,1, ...,4
+//4,3,...,0
+for (let i = jones.length - 1; i >= 0; i--) {
+  console.log(i, jones[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`-------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+  }
+}
