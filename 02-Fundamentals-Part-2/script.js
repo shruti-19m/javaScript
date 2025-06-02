9aa5f4b931f20d0cd6d074671a0c64571aa2e879
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-02 18:35:32 +0530

    upload

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -50,4 +50,30 @@ const appleJuice = fruitprocessor(5,0);
  */
 
 
- 
+//Arrow function
+const calcAge = birthYeah => 2037 - birthYeah;
+const age3 = calcAge(1991);
+console.log(age3);
+
+const yearsUntilRetirement = birthYeah =>{
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  return '${firstName} retires in ${retirement}years';
+
+
+}
+console.log(yearsUntilRetirement(1991, 'jones'));
+console.log(yearsUntilRetirement(1998,'Ruby'));
+
+
+function cutFruitPieces(fruit){
+  return fruit * 4;
+}
+function fruitProcessor(apples,oranges){
+  const applePieces = cutFruitPieces(apples);
+   const orangePieces = cutFruitPieces(oranges);
+
+   const juice= 'Juice with ${applepieces}piece of apple and ${orangepieces}piece of orange';
+   return juice;
+}
+console.log(fruitProcessor(2,3));
