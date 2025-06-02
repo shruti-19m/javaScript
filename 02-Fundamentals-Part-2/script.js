95992b1a0cad218443e0b9065e0c2f360ca1b214
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-03 01:50:30 +0530

    comment

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -394,7 +394,7 @@ for (let exercise = 1; exercise < 4; exercise++) {
 }
   */
 
-
+/*
 // WHILE LOOP
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep}`);
@@ -413,3 +413,5 @@ while (dice !== 6) {
   dice = Math.trunc(Math.random() * 6) + 1;
   if (dice === 6) console.log('Loop is about to end...');
 }
+*/
+
