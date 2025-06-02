4d918a143ea58ec02eed4252ec92be12679938ff
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-02 16:30:41 +0530

    upload

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -7,4 +7,25 @@ if(passTest) hasDriversLicense = true;
 if(hasDriversLicense) console.log('I can drive:D');
 */
 
+function logger(){
+    console.log('My name is shruti');
+}
+ // calling/running/invoking function 
+ // const interface = 'Audio';
+//const private = 537;
 
+logger();
+logger();
+logger();
+
+function fruitprocessor(apples,oranges){
+    console.log(apples,oranges);
+    const juice ='juice with ${apples} apples and ${orange} oranges';
+    return juice;
+     }
+const appleJuice = fruitprocessor(5,0);
+ console.log(appleJuice); 
+
+ const appleorangeJuice = fruitprocessor(3,6);
+ console.log(appleorangeJuice); 
+ 
