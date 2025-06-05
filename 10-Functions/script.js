97db85ea0850c90c0abd8e75b7b589bbecd686f1
shruti-19m <shrutimandal97293512@gmail.com>
2025-06-05 23:01:15 +0530

    upload

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bookings =[];
+
+const createBooking = function(flightNum,numPassengers,price) {
+    const booking ={
+        flightNum,
+        numPassengers,
+        price,
+    };
+    console.log(booking);
+    bookings.push(booking);
+};
+    console.log (bookings);
+    bookings.push(booking);
+
